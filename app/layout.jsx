0f9a240e1f03eb27c59fcb607bac995dd6e4e550
Michael McLean <g4m3rm1k3@hotmail.com>
2023-08-08 05:06:18 -0400

    fisrt commit

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+	return (
+		<html lang='en'>
+			<body>
+				<header>[Header]</header>
+				<main>{children}</main>
+				<footer>[Footer]</footer>
+			</body>
+		</html>
+	);
+}
